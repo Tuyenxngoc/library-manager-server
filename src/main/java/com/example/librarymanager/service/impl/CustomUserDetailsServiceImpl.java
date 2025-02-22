bcade6f137718c417147b498ded5b772144e4b7e
tuyenxngoc <tuyenn9448@gmail.com>
2025-02-22 21:25:15 +0700

    feat: add transactional support to user loading methods in CustomUserDetailsServiceImpl

--- src/main/java/com/example/librarymanager/service/impl/CustomUserDetailsServiceImpl.java
+++ src/main/java/com/example/librarymanager/service/impl/CustomUserDetailsServiceImpl.java
@@ -7,6 +7,7 @@ import com.example.librarymanager.repository.ReaderRepository;
 import com.example.librarymanager.repository.UserRepository;
 import com.example.librarymanager.security.UserDetailsFactory;
 import com.example.librarymanager.service.CustomUserDetailsService;
+import jakarta.transaction.Transactional;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -28,6 +29,7 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
     MessageSource messageSource;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String usernameOrCardNumber) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(usernameOrCardNumber).orElse(null);
         if (user != null) {
@@ -41,6 +43,7 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByUserId(String userId) throws UsernameNotFoundException {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new UsernameNotFoundException(messageSource.getMessage(ErrorMessage.User.ERR_NOT_FOUND_ID, new String[]{userId}, LocaleContextHolder.getLocale())));
@@ -49,6 +52,7 @@ public class CustomUserDetailsServiceImpl implements CustomUserDetailsService {
     }
 
     @Override
+    @Transactional
     public UserDetails loadUserByCardNumber(String cardNumber) throws UsernameNotFoundException {
         Reader reader = readerRepository.findByCardNumber(cardNumber)
                 .orElseThrow(() -> new UsernameNotFoundException(messageSource.getMessage(ErrorMessage.Reader.ERR_NOT_FOUND_CARD_NUMBER, new String[]{cardNumber}, LocaleContextHolder.getLocale())));
