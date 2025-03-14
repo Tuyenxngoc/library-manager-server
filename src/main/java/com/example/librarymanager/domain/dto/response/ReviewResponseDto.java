9fc69de3a63fe7f96a93121e65dba89d69f60092
tuyenxngoc <tuyenn9448@gmail.com>
2025-03-14 09:59:16 +0700

    refactor: update ReviewResponseDto.java

--- src/main/java/com/example/librarymanager/domain/dto/response/ReviewResponseDto.java
+++ src/main/java/com/example/librarymanager/domain/dto/response/ReviewResponseDto.java
@@ -1,11 +1,12 @@
 package com.example.librarymanager.domain.dto.response;
 
+import com.example.librarymanager.domain.dto.common.DateAuditingDto;
 import com.example.librarymanager.domain.dto.response.reader.ReaderBasicResponseDto;
 import com.example.librarymanager.domain.entity.Review;
 import lombok.Getter;
 
 @Getter
-public class ReviewResponseDto {
+public class ReviewResponseDto extends DateAuditingDto {
 
     private final long id;
 
@@ -16,6 +17,8 @@ public class ReviewResponseDto {
     private final ReaderBasicResponseDto reader;
 
     public ReviewResponseDto(Review review) {
+        this.createdDate = review.getCreatedDate();
+        this.lastModifiedDate = review.getLastModifiedDate();
         this.id = review.getId();
         this.rating = review.getRating();
         this.comment = review.getComment();
